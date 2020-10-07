6fcc1f2c2b96c3ccc595dd2674e1c85cf1d197b1
Kevin Yang <kevinyang.cyen@gmail.com>
2020-10-07 14:13:48 -0700

    incorporated post and get requests to /tweet to display tweets in database on page load

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,44 +3,15 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
 
 // loops through tweets
 // calls createTweetElement for each tweet
 // takes return value and appends it to the tweets container
 const renderTweets = function(tweets) {
   for (const tweet in tweets) {
-
     const tweetHTML = createTweetElement(tweets[tweet]);
-    console.log(tweetHTML);
-    console.log($('#tweet-container'));
-
     $('#tweet-container').append(tweetHTML);
-    console.log($('tweet-container'));
   }
 }
 
@@ -71,5 +42,14 @@ const createTweetElement = function(tweet) {
 }
 
 $(document).ready(function() {
-  renderTweets(data);
+
+  $(".tweet-form").submit(function(event) {
+    event.preventDefault();
+    $.post('/tweets', $(this).serialize());
+  });
+
+  $.ajax('/tweets', { method: 'GET' })
+  .then(function(data) {
+    renderTweets(data);
+  });
 });
